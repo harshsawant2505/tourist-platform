6c210002f011829d6eb57aa3f6a7f6c70cb903d4
Harsh Sawant <harshsawant2505@gmail.com>
2024-10-10 21:11:56 +0530

    shaun commit 2

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -4,6 +4,8 @@ import { SafeAreaView } from 'react-native-safe-area-context'
 import { BlurView } from 'expo-blur';
 import Badge from '../components/Badge';
 
+//this is the home screen
+
 const HomeScreen = () => {
 
   return (
